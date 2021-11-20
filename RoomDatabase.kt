b9874ddad34be99bed8668d6d863c3c0b9885c06
Ayana Bando <ayanabando@gmail.com>
2021-11-20 08:06:46 -0500

    Update RoomDatabase.kt

--- RoomDatabase.kt
+++ RoomDatabase.kt
@@ -1,8 +1,8 @@
 @Database(entities = [ItemRoomDatabase::class], version = 1, exportSchema = false)
-
 abstract class ItemRoomDatabase : RoomDatabase() {
 //Remove parentheses and enter Dao Class name
     abstract fun itemDao() : "$DAO_CLASS_NAME$"
+    
     companion object{
          @Volatile
          private var INSTANCE: ItemRoomDatabase? = null
